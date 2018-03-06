9ff3173eab2cfc30f41bdad6f7005ed1a2574dcb
Paul Chelarescu <paul-constantin.chelarescu@student.manchester.ac.uk>
2018-03-06 18:14:36 +0000

    Compute cost multi variables

--- gradientDescentMulti.m
+++ gradientDescentMulti.m
@@ -6,7 +6,6 @@ function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
-theta
 
 for iter = 1:num_iters
 
