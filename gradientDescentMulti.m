d5e49f1a0de635feabeca69763bb4638e7e1fc5a
Paul Chelarescu <paul-constantin.chelarescu@student.manchester.ac.uk>
2018-03-06 18:10:22 +0000

    Removed debugging statement

--- gradientDescentMulti.m
+++ gradientDescentMulti.m
@@ -17,8 +17,8 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-    sum(((X * theta) - y) .* X)
-    theta = theta - alpha * (1 / m) * (sum(((X * theta) - y) .* X))'
+
+    theta = theta - alpha * (1 / m) * (sum(((X * theta) - y) .* X))';
 
 
 
