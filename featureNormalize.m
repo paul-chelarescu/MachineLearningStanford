3f2de1f1dc9d2f24dff205743a4093ab3f388e6b
Paul Chelarescu <paul-constantin.chelarescu@student.manchester.ac.uk>
2018-03-06 17:50:49 +0000

    Finished graded and first optional

--- featureNormalize.m
+++ featureNormalize.m
@@ -26,13 +26,10 @@ sigma = zeros(1, size(X, 2));
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
 
-
-
-
-A = eye(5);
-
-
-
+m = size(X, 1);
+mu = mean(X, 1);
+sigma = std(X, 1);
+X_norm = (X - ones(m, 1) * mu) ./ (ones(m, 1) * sigma);
 
 % ============================================================
 
--- gradientDescent.m
+++ gradientDescent.m
@@ -16,11 +16,14 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
+    % sum(((X * theta) - y) .* X(:, 1))
+    theta1 = theta(1,1) - alpha * (1 / m) * sum(((X * theta) - y) .* X(:, 1));
 
+    theta2 = theta(2,1) - alpha * (1 / m) * sum(((X * theta) - y) .* X(:, 2));
 
-
-
-
+    % theta(1, 1) = theta1
+    % theta(1, 2) = theta2
+    theta = [theta1; theta2];
 
 
     % ============================================================
