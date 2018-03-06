ace82a0b729f947d943be216f567c8790f516d89
Paul Chelarescu <paul-constantin.chelarescu@student.manchester.ac.uk>
2018-03-06 18:09:42 +0000

    Gradient descent multiple variables

--- gradientDescentMulti.m
+++ gradientDescentMulti.m
@@ -6,6 +6,7 @@ function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
+theta
 
 for iter = 1:num_iters
 
@@ -16,8 +17,8 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-
+    sum(((X * theta) - y) .* X)
+    theta = theta - alpha * (1 / m) * (sum(((X * theta) - y) .* X))'
 
 
 
