5e780c8135537c605f601ad1fa455d03764f64cc
Paul Chelarescu <paulchelarescu@gmail.com>
2018-03-17 17:03:30 +0000

    Regularized cost function and gradients

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -36,6 +36,11 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+J = (1 / m) * sum(-y .* log(sigmoid(X * theta)) - (1 - y) .* log(1 - sigmoid(X * theta)))...
+        + (lambda / (2 * m)) * sum(theta(2 : end) .^ 2);
+grad = (1 / m) * X' * (sigmoid(X * theta) - y);
+grad(2 : end) += (lambda / m) * theta(2 : end);
+
 
 
 
